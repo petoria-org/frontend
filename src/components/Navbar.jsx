57a43cc7722c9596da31fdad2d1612a5283cb061
bahar-jafarnejad <jafarnejadbahar3@gmail.com>
2025-11-21 17:35:11 +0330

    add files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='Navbar_background'>
+      <div>
+        
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Stats from "../components/Stats";
+import SuccessStories from "../components/SuccessStories";
+import NewPosts from "../components/NewPosts";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-100 min-h-screen pb-20">
+
+      <Navbar />
+
+      <div>
+        <Hero />
+        <Stats />
+        <SuccessStories />
+        <NewPosts />
+      </div>
+
+    </div>
+  );
+}
