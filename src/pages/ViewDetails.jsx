a0011f878be3398371e09767b2da87e10aaa4707
bahar-jafarnejad <jafarnejadbahar3@gmail.com>
2025-11-25 00:32:58 +0330

    starting

---
+++ src/pages/ViewDetails.jsx
@@ -0,0 +1,11 @@
+import { Navbar_SignIn } from "../components/Navbar_SignIn.jsx";
+import "../styles/postCard.css";
+
+export default function ViewDetails() {
+  return (
+    <div>
+      <Navbar_SignIn />
+      <h1>View Details Page</h1>
+    </div>
+  );
+}
