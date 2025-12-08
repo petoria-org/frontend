fc355d90fcedb06813c911bdfd070a17f1010107
Sara-Nasher <saranasher83@gmail.com>
2025-12-08 11:34:47 +0330

    add post details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Route , Routes } from 'react-router-dom'
 import { PostDetails } from "./pages/PostDetails";
 
-createRoot(document.getElementById("app")).render(
-  <StrictMode>
-    <PostDetails />
-  </StrictMode>,
-);
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route path='/post-details' element={<PostDetails />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
