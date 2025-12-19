96d2d47273f4151fa6140a27a3896d4aab6290b9
bahar-jafarnejad <jafarnejadbahar3@gmail.com>
2025-12-20 00:05:13 +0330

    debug

--- src/components/SuccessStoriesAll/SuccessStoriesAll.jsx
+++ src/components/SuccessStoriesAll/SuccessStoriesAll.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/SuccessStories.css";
+import "../../styles/SuccessStoriesAll.css";
 
 export default function SuccessStories() {
   const navigate = useNavigate();
