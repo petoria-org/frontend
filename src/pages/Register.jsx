52aacfa47c5dee01562571db58d061af9375c519
Asa <asabayatnaderi05@gmail.com>
2025-12-25 22:34:35 +0330

    updated Auth

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import "../styles/register.css";
+import "../styles/Register.css";
 import "../styles/AuthCommon.css";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
@@ -86,133 +86,130 @@ const Register = () => {
     });
   };
 
-
   return (
-    <div className="register-page">
-      <div className="register-container">
-        <div className="register-main">
-          <div className="register-card">
-            <h2 className="register-title">ثبت نام</h2>
-            <p className="register-subtitle">حساب کاربری جدید بسازید</p>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              {/* First name */}
-              <label className="field-label">
-                  نام*
-              </label>
-              <div className="input-wrapper">
-                <img className="input-icon" src="/src/icons/user.svg" alt="user" />
-                <input
-                  type="text"
-                  className="text-input"
-                  placeholder="نام خود را وارد کنید"
-                  {...register("firstName")}
-                />
-              </div>
-              <p className="error">{errors.firstName?.message}</p>
+    <div className="auth-page">
+      <div className="register-main">
+        <div className="register-card">
+          <h2 className="auth-title">ثبت نام</h2>
+          <p className="auth-subtitle">حساب کاربری جدید بسازید</p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {/* First name */}
+            <label className="field-label">
+                نام*
+            </label>
+            <div className="input-wrapper">
+              <img className="input-icon" src="/src/icons/user.svg" alt="user" />
+              <input
+                type="text"
+                className="text-input"
+                placeholder="نام خود را وارد کنید"
+                {...register("firstName")}
+              />
+            </div>
+            <p className="error">{errors.firstName?.message}</p>
 
-              {/* Last name */}
-              <label className="field-label">
-                  نام خانوادگی*
-              </label>
-              <div className="input-wrapper">
-                <img className="input-icon" src="/src/icons/user.svg" alt="user" />
-                <input
-                  type="text"
-                  className="text-input"
-                  placeholder="نام خانوادگی خود را وارد کنید"
-                  {...register("lastName")}
-                />
-              </div>
-              <p className="error">{errors.lastName?.message}</p>
-              
-              {/* Username */}
-              <label className="field-label">
-                  نام کاربری*
-              </label>
-              <div className="input-wrapper">
-                <img className="input-icon" src="/src/icons/profile-circle.svg" alt="profile-circle" />
-                <input
-                  type="text"
-                  className="text-input"
-                  placeholder="نام کاربری خود را وارد کنید"
-                  {...register("username")}
-                />
-              </div>
-              <p className="error">{errors.username?.message}</p>
+            {/* Last name */}
+            <label className="field-label">
+                نام خانوادگی*
+            </label>
+            <div className="input-wrapper">
+              <img className="input-icon" src="/src/icons/user.svg" alt="user" />
+              <input
+                type="text"
+                className="text-input"
+                placeholder="نام خانوادگی خود را وارد کنید"
+                {...register("lastName")}
+              />
+            </div>
+            <p className="error">{errors.lastName?.message}</p>
+            
+            {/* Username */}
+            <label className="field-label">
+                نام کاربری*
+            </label>
+            <div className="input-wrapper">
+              <img className="input-icon" src="/src/icons/profile-circle.svg" alt="profile-circle" />
+              <input
+                type="text"
+                className="text-input"
+                placeholder="نام کاربری خود را وارد کنید"
+                {...register("username")}
+              />
+            </div>
+            <p className="error">{errors.username?.message}</p>
 
-              {/* Email */}
-              <label className="field-label">
-                  ایمیل*
-              </label>
-              <div className="input-wrapper">
-                <img className="input-icon" src="/src/icons/sms.svg" alt="sms" />
-                <input
-                  className="text-input"
-                  placeholder="example@gmail.com"
-                  {...register("email")}
-                />
-              </div>
-              <p className="error">{errors.email?.message}</p>
+            {/* Email */}
+            <label className="field-label">
+                ایمیل*
+            </label>
+            <div className="input-wrapper">
+              <img className="input-icon" src="/src/icons/sms.svg" alt="sms" />
+              <input
+                className="text-input"
+                placeholder="example@gmail.com"
+                {...register("email")}
+              />
+            </div>
+            <p className="error">{errors.email?.message}</p>
 
-              {/* Password */}
-              <label className="field-label">
-                  رمز عبور*
-              </label>
-              <div className="input-wrapper">
-                <img className="input-icon" src="/src/icons/lock.svg" alt="lock" />
-                <input
-                  type="password"
-                  className="text-input"
-                  placeholder="حداقل 8 کاراکتر"
-                  {...register("password")}
-                />
-              </div>
-              <p className="error">{errors.password?.message}</p>
+            {/* Password */}
+            <label className="field-label">
+                رمز عبور*
+            </label>
+            <div className="input-wrapper">
+              <img className="input-icon" src="/src/icons/lock.svg" alt="lock" />
+              <input
+                type="password"
+                className="text-input"
+                placeholder="حداقل 8 کاراکتر"
+                {...register("password")}
+              />
+            </div>
+            <p className="error">{errors.password?.message}</p>
 
-              {/* Confirm password */}
-              <label className="field-label">
-                  تأیید رمز عبور*
-              </label>
-              <div className="input-wrapper">
-                <img className="input-icon" src="/src/icons/lock.svg" alt="lock" />
-                <input
-                  type="password"
-                  className="text-input"
-                  placeholder="رمز عبور را دوباره وارد کنید"
-                  {...register("confirmPassword")}
-                />
-              </div>
-              <p className="error">{errors.confirmPassword?.message}</p>
+            {/* Confirm password */}
+            <label className="field-label">
+                تأیید رمز عبور*
+            </label>
+            <div className="input-wrapper">
+              <img className="input-icon" src="/src/icons/lock.svg" alt="lock" />
+              <input
+                type="password"
+                className="text-input"
+                placeholder="رمز عبور را دوباره وارد کنید"
+                {...register("confirmPassword")}
+              />
+            </div>
+            <p className="error">{errors.confirmPassword?.message}</p>
 
-              {/* Register button */}
-              <button className="register-btn" disabled={loading}>
-                <span>{loading ? "در حال ارسال..." : "ثبت نام"}</span>
-                <img className="arrow" src="/src/icons/arrow-right.svg" alt="arrow-right" />
-              </button>
-            </form>
-
-            {/* divider */}
-              <div className="divider">
-                <span className="divider-line" />
-                <span className="divider-text">یا</span>
-                <span className="divider-line" />
-              </div>
-
-            {/* Google */}
-            <button className="google-btn">
-              <span>ثبت نام با حساب گوگل</span>
-              <img src="/src/icons/chrome.svg" alt="chrome-icon" />
+            {/* Register button */}
+            <button className="register-btn" disabled={loading}>
+              <span>{loading ? "در حال ارسال..." : "ثبت نام"}</span>
+              <img className="arrow" src="/src/icons/arrow-right.svg" alt="arrow-right" />
             </button>
+          </form>
 
-            {/* Already have account */}
-            <p className="register-footer">
-              قبلاً ثبت نام کرده‌اید؟{" "}
-              <Link className="register-footer-link" to={"/login"}>وارد شوید</Link>
-            </p>
-          </div>
-          <div className="register-img">
-            <img src="/src/images/cat.svg" alt="cat" />
-          </div>
+          {/* divider */}
+            <div className="divider">
+              <span className="divider-line" />
+              <span className="divider-text">یا</span>
+              <span className="divider-line" />
+            </div>
+
+          {/* Google */}
+          <button className="google-btn">
+            <span>ثبت نام با حساب گوگل</span>
+            <img src="/src/icons/chrome.svg" alt="chrome-icon" />
+          </button>
+
+          {/* Already have account */}
+          <p className="register-footer">
+            قبلاً ثبت نام کرده‌اید؟{" "}
+            <Link className="register-footer-link" to={"/login"}>وارد شوید</Link>
+          </p>
+        </div>
+        <div className="register-img">
+          <img src="/src/images/cat.svg" alt="cat" />
         </div>
       </div>
     </div>
