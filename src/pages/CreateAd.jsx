088f84137c855a2a5338cb6d3115aa269043f6f4
Asa <asabayatnaderi05@gmail.com>
2025-12-26 03:18:50 +0330

    updated some css files

---
+++ src/pages/CreateAd.jsx
@@ -0,0 +1,11 @@
+import { NotificationOptionsSection } from "../components/NotificationOptionsSection";
+
+const CreateAd = () => {
+  return (
+    <div className="create-ad-page">
+      <NotificationOptionsSection mode="create" />
+    </div>
+  );
+};
+
+export default CreateAd;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Hero from "../components/Hero";
 import Stats from "../components/Stats";
 import SucccessStories from "../components/SuccessStories";
 import NewPosts from "../components/NewPosts";
-import "../styles/Home.css";
 
 export default function Home() {
 
