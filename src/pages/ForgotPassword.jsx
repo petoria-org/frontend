f8ba3a694bc9f858d3d963df112aa18c086c8dc6
arshiasamimi <samimarsh44@gmail.com>
2025-12-27 20:04:02 +0330

    fixed import errors

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,4 +1,4 @@
-import "../styles/ForgotPassword.css";
+import "../styles/forgotPassword.css";
 import "../styles/AuthCommon.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,4 +1,4 @@
-import "../styles/ResetPassword.css";
+import "../styles/resetPassword.css";
 import "../styles/AuthCommon.css";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
