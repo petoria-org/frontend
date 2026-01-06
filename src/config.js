e11d81a60d51ae51bdd23871271d86ba66dc185d
arshiasamimi <samimarsh44@gmail.com>
2026-01-06 17:22:45 +0330

    minor url fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 export const config = {
-  BACKEND_URL: "https://petoria-back.liara.run/",
+  BACKEND_URL: "https://petoria-back.liara.run",
   API_BASE_URL: "https://petoria-back.liara.run/api",
   MEDIA_URL: "https://petoria-back.liara.run/media",
 };
